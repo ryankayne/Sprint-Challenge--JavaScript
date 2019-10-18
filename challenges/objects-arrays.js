03f152f83480ad4644248726ac6c6bf3f29b859b
ryankayne <coope1rk@gmail.com>
2019-10-18 13:29:04 -0400

    Finished Short Answer Qs

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -100,7 +100,8 @@ console.log(contactInfo);
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
 graduates.forEach(uni =>{
-  unisWithUni.push (`${uni.university}`);
+
+  // unisWithUni.push (`${uni.university}`);
   unisWithUni.includes ('Uni');
 });
 
